238475863a12c769ef1069df624bdf79c0ecc607
Donpaul Oduor <donpaul.oduordonpaul@gmail.com>
2024-02-21 00:30:43 -0800

    updated classes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models import storage_type
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+
+    __tablename__ = "states"
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state', cascade='all, delete')
+    else:
+        name = ''
+
+        @property
+        def cities(self):
+            from models.city import City
+            from models import storage
+            linked_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    linked_cities.append(city)
+            return linked_cities
